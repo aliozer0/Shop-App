d0fefb14a5469f5e877234e707ba9eba7ecc7e19
aliozer00 <92879108+aliozer00@users.noreply.github.com>
2023-04-09 22:07:58 +0300

    submitting Forms

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -11,11 +11,42 @@ class EditProductScren extends StatefulWidget {
 
 class _EditProductScrenState extends State<EditProductScren> {
   final _priceFocusNode = FocusNode();
+  final _descriptionFocusNode = FocusNode();
+  final _imageUrlController = TextEditingController();
+  final _imageFocusNode = FocusNode();
+  final _form = GlobalKey();
+
+  @override
+  void dispose() {
+    _priceFocusNode.dispose();
+    _descriptionFocusNode.dispose();
+    _imageUrlController.dispose();
+    _imageFocusNode.dispose();
+    _imageFocusNode.removeListener(_updateImageUrl);
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    _imageFocusNode.addListener(_updateImageUrl);
+    super.initState();
+  }
+
+  void _updateImageUrl() {
+    if (!_imageFocusNode.hasFocus) {
+      setState(() {});
+    }
+  }
+
+  void _saveForm() {}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Edit Products')),
+      appBar: AppBar(
+        title: Text('Edit Products'),
+        actions: [IconButton(onPressed: _saveForm, icon: Icon(Icons.save))],
+      ),
       body: Padding(
         padding: const EdgeInsets.all(15.0),
         child: Form(
@@ -33,6 +64,48 @@ class _EditProductScrenState extends State<EditProductScren> {
               textInputAction: TextInputAction.next,
               keyboardType: TextInputType.number,
               focusNode: _priceFocusNode,
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Description'),
+              maxLength: 3,
+              keyboardType: TextInputType.multiline,
+              focusNode: _descriptionFocusNode,
+              onFieldSubmitted: (value) {
+                FocusScope.of(context).requestFocus(_descriptionFocusNode);
+              },
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  margin: EdgeInsets.only(top: 8, right: 10),
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 1, color: Colors.grey),
+                  ),
+                  child: _imageUrlController.text.isEmpty
+                      ? Text('Enter a URL')
+                      : FittedBox(
+                          child: Image.network(
+                            _imageUrlController.text,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                ),
+                Expanded(
+                  child: TextFormField(
+                    decoration: InputDecoration(labelText: 'Image URL'),
+                    keyboardType: TextInputType.url,
+                    textInputAction: TextInputAction.done,
+                    controller: _imageUrlController,
+                    focusNode: _imageFocusNode,
+                    onFieldSubmitted: (_) {
+                      _saveForm();
+                    },
+                  ),
+                ),
+              ],
             )
           ],
         )),
